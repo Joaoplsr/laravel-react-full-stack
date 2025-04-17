2e2e2e9bdc7fe5f48add1334872b26cfc2dc0f88
João Pedro Leal <168142462+Joaoplsr@users.noreply.github.com>
2025-04-16 22:52:58 -0300

    Imports

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,3 +1,5 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout() {
   const { user, token } = useStateContext()
